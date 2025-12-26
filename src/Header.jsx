2079d58d79a117bea42f479f377516c210c46b3e
mithu <golamrabbani3010@gmail.com>
2025-12-27 00:51:15 +0600

    Refactor code structure for improved readability and maintainability

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import Logo from "./assets/logo.png";
+
+export default function Header() {
+  return (
+    <nav className="py-6 md:py-4 fixed top-0 w-full !bg-[#191D26] z-50">
+      <div className="container mx-auto flex items-center justify-between gap-x-6">
+        <a href="/">
+          <img className="h-[70px]" src={Logo} alt="Logo" />
+        </a>
+      </div>
+    </nav>
+  );
+}
